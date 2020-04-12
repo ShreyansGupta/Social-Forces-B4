1e4d47d65e75ff3f5fed55a34debc1b9e5999daa
ShreyansGupta <50500026+ShreyansGupta@users.noreply.github.com>
2020-04-12 11:26:29 -0400

    Leader Follower

---
+++ Assets/Scripts/Leader.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Leader : MonoBehaviour
+{
+    public float radius;
+    public float mass;
+    public float perceptionRadius;
+
+    private List<Vector3> path;
+    private NavMeshAgent nma;
+    private Rigidbody rb;
+    private float leaderDist=1.5f;
+
+    private HashSet<GameObject> perceivedNeighbors = new HashSet<GameObject>();
+
+
+    public void Start()
+    {
+        path = new List<Vector3>();
+        nma = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+
+        gameObject.transform.localScale = new Vector3(2 * radius, 1, 2 * radius);
+        nma.radius = radius;
+        rb.mass = mass;
+        GetComponent<SphereCollider>().radius = perceptionRadius / 2;
+
+    }
+
+    public void Update()
+    {
+        if (path.Count > 1 && Vector3.Distance(transform.position, path[0]) < 1.1f)
+        {
+            path.RemoveAt(0);
+        }
+        else if (path.Count == 1 && Vector3.Distance(transform.position, path[0]) < 1.5f)
+        {
+            path.RemoveAt(0);
+
+            if (path.Count == 0)
+            {
+               /* gameObject.SetActive(false);
+                AgentManager.RemoveAgent(gameObject);*/
+            }
+        }
+
+        #region Visualization
+
+        if (false)
+        {
+            if (path.Count > 0)
+            {
+                Debug.DrawLine(transform.position, path[0], Color.green);
+            }
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                Debug.DrawLine(path[i], path[i + 1], Color.yellow);
+            }
+        }
+
+        if (false)
+        {
+            foreach (var neighbor in perceivedNeighbors)
+            {
+                Debug.DrawLine(transform.position, neighbor.transform.position, Color.yellow);
+            }
+        }
+
+        #endregion
+    }
+
+    #region Public Functions
+
+    public void ComputePath(Vector3 destination)
+    {
+        //Debug.Log("Compute Path Called");
+        nma.enabled = true;
+        var nmPath = new NavMeshPath();
+        nma.CalculatePath(destination, nmPath);
+        path = nmPath.corners.Skip(1).ToList();
+        //path = new List<Vector3>() { destination };
+        //nma.SetDestination(destination);
+        nma.enabled = false;
+    }
+
+    public Vector3 GetVelocity()
+    {
+        return rb.velocity;
+    }
+
+    #endregion
+
+    #region Incomplete Functions
+
+    private Vector3 ComputeForce()
+    {
+        var force = Vector3.zero;
+        var leader = LeaderManager.leader;
+        bool isLeader = int.Parse(name.Split(' ')[1]) == 1;
+        if (isLeader)
+        {
+            if (path.Count == 1 && Vector3.Distance(transform.position, path[0]) > 1.5f)
+                force += CalculateGoalForce()*0.01f;
+            else
+            {
+                this.rb.velocity = Vector3.zero;
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position,leader.transform.position)>3.0f)
+                force += CalculateLeaderForce(leader);
+            else
+            {
+                rb.velocity = Vector3.zero;
+                
+            }
+        }
+        if (force != Vector3.zero)
+        {
+            force.y = 0;
+            return force.normalized * Mathf.Min(force.magnitude,3f);
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+    protected virtual Vector3 CalculateGoalForce()
+    {
+        if (path.Count == 0)
+        {
+            return Vector3.zero;
+        }
+        var desiredVel = (path[0] - transform.position);
+
+        // var desiredVel = desiredVel.normalized * Mathf.Min(desiredDirect.magnitude, Parameters.maxSpeed);
+        var calculateAcc = (desiredVel - this.GetVelocity()) / Parameters.T;
+
+        return mass * calculateAcc;
+
+    }
+        private Vector3 CalculateLeaderForce(GameObject leader)
+    {
+
+        var agentForce = Vector3.zero;
+        var direction = (transform.position - leader.transform.position).normalized;
+        //agentForce -= (direction) * 0.5f;
+
+        var leaderVel = leader.GetComponent<Leader>().GetVelocity();
+        leaderVel = leaderVel.normalized * leaderDist;
+        var aheadPos = leader.transform.position + leaderVel;
+        var onWay = Vector3.Distance(aheadPos, transform.position) < 1.5f || Vector3.Distance(leader.transform.position, transform.position) < 1.5f;
+        if (onWay)
+        {
+            Debug.Log("On the way");
+            transform.position = aheadPos + Vector3.forward*1.5f;
+        }
+        //var direction = (transform.position - aheadPos).normalized;
+        //var tangent = Vector3.Cross(Vector3.up, direction).normalized;
+        agentForce -= (direction) * 0.1f;
+        //agentForce += tangent * 0.01f;
+
+        
+
+        Debug.DrawRay(transform.position, agentForce, Color.red);
+        return agentForce;
+        
+    }
+    public void ApplyForce()
+    {
+        var force = ComputeForce();
+        force.y = 0;
+
+        rb.AddForce(force * 10, ForceMode.Force);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (LeaderManager.IsAgent(other.gameObject) && other.gameObject!=LeaderManager.leader)
+        {
+            perceivedNeighbors.Add(other.gameObject);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (perceivedNeighbors.Contains(other.gameObject))
+        {
+            perceivedNeighbors.Remove(other.gameObject);
+        }
+    }
+
+   
+    #endregion
+}
---
+++ Assets/Scripts/LeaderManager.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LeaderManager : MonoBehaviour
+{
+    public int agentCount = 10;
+    public float agentSpawnRadius = 20;
+    public GameObject agentPrefab;
+    public static Dictionary<GameObject, Leader> agentsObjs = new Dictionary<GameObject, Leader>();
+    public Material PursuerMaterial;
+    
+    public static GameObject leader;
+
+    private static List<Leader> agents = new List<Leader>();
+    private GameObject agentParent;
+    private Vector3 destination = Vector3.forward;
+
+
+    public const float UPDATE_RATE = 0.0f;
+    private const int PATHFINDING_FRAME_SKIP = 25;
+
+    #region Unity Functions
+
+    void Awake()
+    {
+
+        Random.InitState(0);
+
+        agentParent = GameObject.Find("Agents");
+        for (int i = 0; i < agentCount; i++)
+        {
+            var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 0, (Random.value - 0.5f) * agentSpawnRadius);
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randPos, out hit, 10, NavMesh.AllAreas);
+            randPos = hit.position + Vector3.up;
+
+            GameObject agent = null;
+            agent = Instantiate(agentPrefab, randPos, Quaternion.identity);
+            agent.name = "Agent " + i;
+            agent.transform.parent = agentParent.transform;
+            var agentScript = agent.GetComponent<Leader>();
+            agentScript.radius = 0.3f;// Random.Range(0.2f, 0.6f);
+            agentScript.mass = 1;
+            agentScript.perceptionRadius = 20;
+            if (i == 1)
+            {
+                agent.GetComponent<MeshRenderer>().material = PursuerMaterial;
+                leader = agent;
+            }
+            
+
+            agents.Add(agentScript);
+            agentsObjs.Add(agent, agentScript);
+        }
+
+        StartCoroutine(Run());
+    }
+
+    void Update()
+    {
+        #region Visualization
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse clicked");
+            if (true)
+            {
+                var point = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+                var dir = point - Camera.main.transform.position;
+                RaycastHit rcHit;
+                if (Physics.Raycast(point, dir, out rcHit))
+                {
+                    point = rcHit.point;
+
+                }
+            }
+            else
+            {
+                var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 0, (Random.value - 0.5f) * agentSpawnRadius);
+
+                NavMeshHit hit;
+                NavMesh.SamplePosition(randPos, out hit, 1.0f, NavMesh.AllAreas);
+                print(hit.position);
+                Debug.DrawLine(hit.position, hit.position + Vector3.up * 10, Color.red, 1000000);
+                foreach (var agent in agents)
+                {
+                    //agent.ComputePath(hit.position);
+                }
+            }
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit1 = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit1)
+            {
+                Debug.Log("Mouse");
+                destination = hitInfo.point;
+                SetAgentDestinations(destination);
+
+            }
+        }
+#if UNITY_EDITOR
+        if (Application.isFocused)
+        {
+            //UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+        }
+#endif
+
+        #endregion
+    }
+
+    IEnumerator Run()
+    {
+        yield return null;
+
+        for (int iterations = 0; ; iterations++)
+        {
+            if (iterations % PATHFINDING_FRAME_SKIP == 0)
+            {
+                //SetAgentDestinations(destination);
+            }
+
+            foreach (var agent in agents)
+            {
+                agent.ApplyForce();
+            }
+
+            if (UPDATE_RATE == 0)
+            {
+                yield return null;
+            }
+            else
+            {
+                yield return new WaitForSeconds(UPDATE_RATE);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Public Functions
+
+    public static bool IsAgent(GameObject obj)
+    {
+        return agentsObjs.ContainsKey(obj);
+    }
+
+    public void SetAgentDestinations(Vector3 destination)
+    {
+        NavMeshHit hit;
+        NavMesh.SamplePosition(destination, out hit, 10, NavMesh.AllAreas);
+        foreach (var agent in agents)
+        {
+            agent.ComputePath(hit.position);
+        }
+        Debug.Log("Destination is set");
+    }
+
+    public static void RemoveAgent(GameObject obj)
+    {
+        var agent = obj.GetComponent<Leader>();
+
+        agents.Remove(agent);
+        agentsObjs.Remove(obj);
+    }
+
+    #endregion
+
+    #region Private Functions
+
+    #endregion
+
+    #region Visualization Functions
+
+    #endregion
+
+    #region Utility Classes
+
+    private class Tuple<K, V>
+    {
+        public K Item1;
+        public V Item2;
+
+        public Tuple(K k, V v)
+        {
+            Item1 = k;
+            Item2 = v;
+        }
+    }
+
+    #endregion
+}
